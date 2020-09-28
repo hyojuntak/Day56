65f9babe7cbb53839d68ccda79296a559daa475f
hyojuntak <67631752+hyojuntak@users.noreply.github.com>
2020-09-28 17:46:08 +0900

    Create RadixTest.java

---
+++ RadixTest.java
@@ -0,0 +1,24 @@
+package JC.Day56;
+
+public class RadixTest {
+    public static void main(String[] args) {
+        String num = "11";
+
+        try {
+            int bin = Integer.parseInt(num,2);
+            int oct = Integer.parseInt(num,8);
+            int dec = Integer.parseInt(num,10);
+            int hex = Integer.parseInt(num,16);
+
+            System.out.println("주어진 값: "+num);
+            System.out.println("2진수로 봤을때: "+bin);
+            System.out.println("8진수로 봤을때: "+oct);
+            System.out.println("10진수로 봤을때: "+dec);
+            System.out.println("16진수로 봤을때: "+hex);
+
+        } catch (Exception e) {
+            //TODO: handle exception
+            System.out.println("ERROR : "+e);
+        }
+    }
+}
