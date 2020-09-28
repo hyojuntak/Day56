d82bddb0fa3c448ea7340abbd6d0375ae0166b51
hyojuntak <67631752+hyojuntak@users.noreply.github.com>
2020-09-28 17:43:09 +0900

    Create AssignmentTest.java

---
+++ AssignmentTest.java
@@ -0,0 +1,17 @@
+package JC.Day56;
+
+public class AssignmentTest {
+    public static void main(String[] args) {
+        int x,y,z;
+
+        x = y = z = 1;
+        z += x+y;
+        System.out.println("x="+x+",y="+y+",z="+z);
+    
+    
+        x+=y-=z = 5;
+        System.out.println("x="+x+",y="+y+",z="+z);
+    
+    
+    }
+}
