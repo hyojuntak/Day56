c0947b9d2f0fc50a752ba44f92d7cccc4860df10
hyojuntak <67631752+hyojuntak@users.noreply.github.com>
2020-09-28 17:44:59 +0900

    Create inputstream.java

---
+++ inputstream.java
@@ -0,0 +1,16 @@
+package JC.Day56;
+import java.io.*;
+
+public class inputstream {
+    public static void main(String[] args) throws IOException {
+        
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader in = new BufferedReader(isr);
+
+        String str;
+        System.out.println("글씨를 입력하면 따라합니다.");
+        str = in.readLine();
+        System.out.println(str);
+  
+    }
+}
